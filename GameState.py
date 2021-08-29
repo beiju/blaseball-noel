a56c1d6210f1ce4f1deadcbd2ef56868cc16222f
beiju <beiju@me.com>
2021-08-29 00:02:56 -0400

    Can do an entire half-inning!

--- GameState.py
+++ GameState.py
@@ -4,6 +4,20 @@ from blaseball_mike.models import Team
 
 from parsers import Parsers
 
+BASE_NUM_FOR_HIT = {
+    'Single': 0,
+    'Double': 1,
+    'Triple': 2,
+    'Quadruple': 3,
+}
+
+BASE_NUM_FOR_NAME = {
+    'first': 0,
+    'second': 1,
+    'third': 2,
+    'fourth': 3,
+}
+
 
 def mod_for_player(player):
     return ""
@@ -28,11 +42,10 @@ class TeamState:
                                  timestamp)
         self.nickname = team.get_nickname()
 
-        self.pitcher = PlayerState(id=first_truthy(updates, prefix + 'Pitcher'),
-                                   name=first_truthy(updates,
-                                                     prefix + 'PitcherName'),
-                                   mod=first_truthy(updates,
-                                                    prefix + 'PitcherMod') or '')
+        self.pitcher = PlayerState(
+            id=first_truthy(updates, prefix + 'Pitcher'),
+            name=first_truthy(updates, prefix + 'PitcherName'),
+            mod=first_truthy(updates, prefix + 'PitcherMod') or '')
         assert self.pitcher.id
         assert self.pitcher.name
 
@@ -55,20 +68,12 @@ class GameState:
     update_type: Dict[int, UpdateFunction] = {}
 
     def __init__(self, updates: List[dict]):
-        """
-        Create a GameState
-        
-        Takes a first_update to get game-specific parameters that should be
-        static for the whole game. This should be the first available update
-        after "Let's Go!" and "Play ball!" 
-        :param game_updates:
-        """
-
         self.expects_lets_go = True
         self.expects_play_ball = False
-        self.expects_inning_start = False
+        self.expects_half_inning_start = False
         self.expects_batter_up = False
         self.expects_pitch = False
+        self.expects_inning_end = False
 
         # Chronicler adds timestamp so I can depend on it existing
         self.away = TeamState(updates, updates[0]['timestamp'], 'away')
@@ -174,6 +179,7 @@ class GameState:
         return self.home if self.game_update['topOfInning'] else self.away
 
     def update(self, feed_event, game_update):
+        print(feed_event['description'])
         update_func = GameState.update_type[feed_event['type']]
         returned = update_func(self, feed_event, game_update)
 
@@ -205,7 +211,7 @@ class GameState:
         assert self.expects_play_ball
         assert feed_event['description'] == "Play ball!"
         self.expects_play_ball = False
-        self.expects_inning_start = True
+        self.expects_half_inning_start = True
 
         self.game_update['phase'] = 2
         self.game_update['inning'] = -1
@@ -230,12 +236,13 @@ class GameState:
 
         return special_game_update
 
-    def update_inning_start(self, feed_event: dict, _: Optional[dict]):
-        assert self.expects_inning_start
+    def update_half_inning_start(self, feed_event: dict, _: Optional[dict]):
+        assert self.expects_half_inning_start
 
         self.game_update['phase'] = 6  # whatever that means
         self.game_update['gameStartPhase'] = 10  # ???
-        self.game_update['inning'] += 1
+        if not self.game_update['topOfInning']:
+            self.game_update['inning'] += 1
         self.game_update['topOfInning'] = not self.game_update['topOfInning']
 
         top_or_bottom = "Top" if self.game_update['topOfInning'] else "Bottom"
@@ -246,7 +253,7 @@ class GameState:
         assert feed_event['description'] == description
         self.game_update['lastUpdate'] = description
 
-        self.expects_inning_start = False
+        self.expects_half_inning_start = False
         self.expects_batter_up = True
 
     def update_batter_up(self, feed_event: dict, _: Optional[dict]):
@@ -270,34 +277,142 @@ class GameState:
         self.expects_batter_up = False
         self.expects_pitch = True
 
-    def update_flyout(self, feed_event: dict, _: Optional[dict]):
-        assert self.expects_pitch
-        parsed = Parsers.flyout.parse(feed_event['description'])
+    def update_flyout(self, feed_event: dict, game_update: Optional[dict]):
+        self._update_fielding_out(feed_event, game_update)
 
-        assert parsed.children[0] == self.batter().name
-        assert any(parsed.children[1] == defender.name
-                   for defender in self.fielding_team().lineup)
+    def update_ground_out(self, feed_event: dict, game_update: Optional[dict]):
+        self._update_fielding_out(feed_event, game_update)
+
+    def _update_fielding_out(self, feed_event: dict, _: Optional[dict]):
+        assert self.expects_pitch
+
+        parsed = Parsers.fielding_out.parse(feed_event['description'])
+
+        batter = self.batter()
+        if parsed.data == 'ground_out' or parsed.data == 'flyout':
+            batter_name, fielder = parsed.children
+            assert any(fielder == defender.name
+                       for defender in self.fielding_team().lineup)
+        else:
+            assert parsed.data == 'fielders_choice'
+            (parsed_out, parsed_reaches) = parsed.children
+            runner_out, base_name = parsed_out.children
+            batter_name, = parsed_reaches.children
+
+            base_i = self.game_update['baseRunnerNames'].index(runner_out)
+            self.game_update['baseRunners'][base_i] = batter.id
+            self.game_update['baseRunnerNames'][base_i] = batter.name
+            self.game_update['baseRunnerMods'][base_i] = batter.mod
+
+        assert batter_name == batter.name
 
         self.game_update['lastUpdate'] = feed_event['description']
+
         self._update_out()
 
     def _update_out(self):
+        self.game_update['halfInningOuts'] += 1
+
+        if (self.game_update['halfInningOuts'] >=  # I see you, Crowvertime
+                self.game_update[self.prefix() + 'Outs']):
+            self._end_half_inning()
+        else:
+            self._end_atbat()
+
+    def _end_atbat(self):
         prefix = self.prefix()
+
         self.game_update[prefix + 'Batter'] = None
         self.game_update[prefix + 'BatterName'] = ''
         self.game_update[prefix + 'BatterMod'] = ''
         self.game_update['atBatBalls'] = 0
         self.game_update['atBatStrikes'] = 0
-        self.game_update['halfInningOuts'] += 1
 
         self.expects_pitch = False
         self.expects_batter_up = True
+
+    def _end_half_inning(self):
+        self._end_atbat()
+
+        self.game_update['baseRunners'] = []
+        self.game_update['baseRunnerNames'] = []
+        self.game_update['baseRunnerMods'] = []
+        self.game_update['baseRunnerMods'] = []
+        self.game_update['basesOccupied'] = []
+        self.game_update['baserunnerCount'] = 0
+
+        self.game_update['halfInningOuts'] = 0
+        self.game_update['phase'] = 3
+
+        self.expects_batter_up = False
+        if self.game_update['topOfInning']:
+            self.expects_half_inning_start = True
+        else:
+            self.expects_inning_end = True
+
+    def update_ball(self, feed_event: dict, _: Optional[dict]):
+        assert self.expects_pitch
+
+        self.game_update['atBatBalls'] += 1
+        self._update_count(feed_event, ["Ball"])
+
+    def update_foul_ball(self, feed_event: dict, _: Optional[dict]):
+        assert self.expects_pitch
+
+        if (self.game_update['atBatStrikes'] + 1 <
+                self.game_update[self.prefix() + 'Strikes']):
+            self.game_update['atBatStrikes'] += 1
+
+        self._update_count(feed_event, ["Foul Ball"])
+
+    def update_strike(self, feed_event: dict, _: Optional[dict]):
+        assert self.expects_pitch
+
+        self.game_update['atBatStrikes'] += 1
+        self._update_count(feed_event, ["Strike, swinging",
+                                        "Strike, looking",
+                                        "Strike, flinching"])
+
+    def _update_count(self, feed_event, text_options):
+        balls = self.game_update['atBatBalls']
+        strikes = self.game_update['atBatStrikes']
+        for text in text_options:
+            description = f"{text}. {balls}-{strikes}"
+
+            if feed_event['description'] == description:
+                self.game_update['lastUpdate'] = description
+                break
+        else:
+            assert False
+
+    def update_hit(self, feed_event: dict, _: Optional[dict]):
+        assert self.expects_pitch
+
+        parsed = Parsers.hit.parse(feed_event['description'])
+        batter = self.batter()
+        assert parsed.children[0] == batter.name
+
+        self._end_atbat()
+
+        base_reached = BASE_NUM_FOR_HIT[parsed.children[1]]
+
+        self.game_update['lastUpdate'] = feed_event['description']
+        self.game_update['baseRunners'].append(batter.id)
+        self.game_update['baseRunnerNames'].append(batter.name)
+        self.game_update['baseRunnerMods'].append(batter.mod)
+        self.game_update['basesOccupied'].append(base_reached)
+        self.game_update['baserunnerCount'] += 1
 
 
 GameState.update_type = {
     0: GameState.update_lets_go,
     1: GameState.update_play_ball,
-    2: GameState.update_inning_start,
+    2: GameState.update_half_inning_start,
     7: GameState.update_flyout,
+    8: GameState.update_ground_out,
+    10: GameState.update_hit,
     12: GameState.update_batter_up,
+    13: GameState.update_strike,
+    14: GameState.update_ball,
+    15: GameState.update_foul_ball,
 }
--- parsers.py
+++ parsers.py
@@ -3,11 +3,14 @@ from lark import Lark
 
 def _make_parser(name):
     with open(f"parser_grammars/{name}.lark", 'r') as f:
-        return Lark(f.read(), start='start', debug=True, lexer='dynamic_complete')
+        return Lark(f.read(), start='start', debug=True,
+                    lexer='dynamic_complete', import_paths=['parser_grammars'])
 
 
 class Parsers:
-    flyout = _make_parser('flyout')
+    ball = _make_parser('ball')
+    hit = _make_parser('hit')
+    fielding_out = _make_parser('fielding_out')
 
     def __init__(self):
         raise RuntimeError("This class should not be instantiated")
